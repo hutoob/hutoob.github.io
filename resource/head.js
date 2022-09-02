37aa5eaf734623a90f095841f52c7c5feb6696b4
三月 <72554633+hutoob@users.noreply.github.com>
2022-09-02 23:56:03 +0800

    Add files via upload

---
+++ resource/head.js
@@ -0,0 +1,12 @@
+function toindex(){
+    location.href="index.html"
+}
+function toresume(){
+    location.href="resume.html"
+}
+function toblog(){
+    location.href="https://www.cnblogs.com/haobox/"
+}
+function togithub(){
+    location.href="https://github.com/hutoob"
+}
